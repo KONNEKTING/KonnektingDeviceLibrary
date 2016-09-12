1dbc4004504ca135a4e256c01e33f8e5db515250
tuxedo0801 <alex@root1.de>
2016-09-12 15:45:49 +0200

    added first "real" external eeprom implementation.

--- examples/DemoSketch/DemoSketch.ino
+++ examples/DemoSketch/DemoSketch.ino
@@ -10,23 +10,23 @@
 #ifdef DEBUG
 #include <DebugUtil.h>
 
-// Get right serial port for debugging
+// Get correct serial port for debugging
 #ifdef __AVR_ATmega32U4__
-    // Leonardo/Micro/ProMicro use the USB serial port
-    #define DEBUGSERIAL Serial
+// Leonardo/Micro/ProMicro use the USB serial port
+#define DEBUGSERIAL Serial
 #elif __SAMD21G18A__ 
-    // Zero use native USB port
-    #define DEBUGSERIAL SerialUSB
+// Zero use native USB port
+#define DEBUGSERIAL SerialUSB
 #elif ESP8266
-    // ESP8266 use the 2nd serial port with TX only
-    #define DEBUGSERIAL Serial1
+// ESP8266 use the 2nd serial port with TX only
+#define DEBUGSERIAL Serial1
 #else
-    // All other, (ATmega328P f.i.) use software serial
-    #include <SoftwareSerial.h>
-    SoftwareSerial softserial(11, 10); // RX, TX
-    #define DEBUGSERIAL softserial
+// All other, (ATmega328P f.i.) use software serial
+#include <SoftwareSerial.h>
+SoftwareSerial softserial(11, 10); // RX, TX
+#define DEBUGSERIAL softserial
 #endif
-    // end of debugging defs
+// end of debugging defs
 #endif
 
 
@@ -48,7 +48,7 @@
 #define PROG_BUTTON_PIN 3
 #define TEST_LED 5 //or change it to another pin
 
-    
+
 // ################################################
 // ### Global variables, sketch related
 // ################################################
@@ -62,48 +62,72 @@ int laststate = false;
 // ### Only required if HW does not support 
 // ### internal eeprom, like Arduino Zero
 // ################################################
+
+// Arduino Zero has no "real" internal EEPROM, 
+// so we use an external, accesses via I2C
+// so we need to include Arduino's Wire.h
+#ifdef __SAMD21G18A__
+#include <Wire.h>
+// Example for 24AA02E64 I2C EEPROM
+
 int readEeprom(int index) {
-    // when using external storage, implement READ command here
-    //return EEPROM.read(index);
-    return 0;
+    byte rdata = 0xFF;
+
+    Wire.beginTransmission(0x50);
+    Wire.write((int) (index));
+    Wire.endTransmission();
+
+    Wire.requestFrom(0x50, 1);
+
+    if (Wire.available()) {
+        rdata = Wire.read();
+    }
+
+    return rdata;
 }
 
 void writeEeprom(int index, int val) {
-    // when using external storage, implement WRITE command here
-    //return EEPROM.write(index, val);
-    return;
+    Wire.beginTransmission(0x50);
+    Wire.write((int) (index));
+    Wire.write(val);
+    Wire.endTransmission();
+
+    delay(5); //is it needed?!
 }
 
 void updateEeprom(int index, int val) {
-    // when using external storage, implement UPDATE command here
-    //return EEPROM.update(index, val);
+    if (readEeprom(index) != val) {
+        writeEeprom(index, val);
+    }
 }
+#endif
 
 
 // ################################################
 // ### SETUP
 // ################################################
+
 void setup() {
 
-// debug related stuff
+    // debug related stuff
 #ifdef DEBUG
-    
+
     // Start debug serial with 9600 bauds
     DEBUGSERIAL.begin(9600);
-    
-#if defined(__AVR_ATmega32U4__) || (__SAMD21G18A__)
+
+#ifdef __AVR_ATmega32U4__ || __SAMD21G18A__
     // wait for serial port to connect. Needed for Leonardo/Micro/ProMicro/Zero only
     while (!DEBUGSERIAL)
 #endif
-        
+
     // make debug serial port known to debug class
     // Means: KONNEKTING will sue the same serial port for console debugging
     Debug.setPrintStream(&DEBUGSERIAL);
 #endif
-    
+
     Debug.print(F("KONNEKTING DemoSketch\n"));
-    
-    pinMode(TEST_LED, OUTPUT);  
+
+    pinMode(TEST_LED, OUTPUT);
 
     /*
      * Only required when using external eeprom (or similar) storage.
@@ -115,7 +139,7 @@ void setup() {
     Konnekting.setEepromWriteFunc(&writeEeprom);
     Konnekting.setEepromUpdateFunc(&updateEeprom);
 #endif    
-    
+
     // Initialize KNX enabled Arduino Board
     Konnekting.init(KNX_SERIAL,
             PROG_BUTTON_PIN,
@@ -123,13 +147,13 @@ void setup() {
             MANUFACTURER_ID,
             DEVICE_ID,
             REVISION);
-    
-    // If device has been parametrized with KONNEKTING Suite, read params from eeprom
-    // Otherwise contibue with global default values from sketch
-    if (!Konnekting.isFactorySetting()){
+
+    // If device has been parametrized with KONNEKTING Suite, read params from EEPROM
+    // Otherwise continue with global default values from sketch
+    if (!Konnekting.isFactorySetting()) {
         diffmillis = (int) Konnekting.getUINT16Param(PARAM_initialDelay); //blink every xxxx ms
     }
-    
+
     lastmillis = millis();
 
     Debug.println(F("Toggle every %d ms."), diffmillis);
@@ -139,34 +163,35 @@ void setup() {
 // ################################################
 // ### LOOP
 // ################################################
+
 void loop() {
-    
+
     // Do KNX related stuff (like sending/receiving KNX telegrams)
     // This is required in every KONNEKTING aplication sketch
     Knx.task();
-    
+
     unsigned long currentmillis = millis();
-    
+
     /*
      * only do measurements and other sketch related stuff if not in programming mode
      * means: only when konnekting is ready for appliction
      */
     if (Konnekting.isReadyForApplication()) {
-        
+
         if (currentmillis - lastmillis >= diffmillis) {
-            
+
             Debug.println(F("Actual state: %d"), laststate);
             Knx.write(COMOBJ_commObj2, laststate);
             laststate = !laststate;
             lastmillis = currentmillis;
-            
+
             digitalWrite(TEST_LED, HIGH);
             Debug.println(F("DONE"));
-            
+
         }
-        
+
     }
-    
+
 }
 
 // ################################################
